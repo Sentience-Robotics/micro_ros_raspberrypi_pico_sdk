77ad955edf1a2e854c0b72c3793e7655a240653f
Antoine ESMAN <antoine.esman@epitech.eu>
2025-10-08 02:40:09 +0100

    feat(I2C): add architecture for i2c control
    feat(DEMO): all fingers connected, logging back when moving servos
    fix(readability): clean code
    fix(ci): delete useless ci
    fix(config): full arm config - fixing GPIO pin offset
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>
    
    Co-authored-by: m-brl <mathieu.borel@epitech.eu>

--- include/pwm.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <hardware/pwm.h>
-
-extern pwm_config config;
-
-void init_pwm(void);
-void init_pwm_port(int16_t pin);
--- src/ros.c
+++ src/ros.c
@@ -20,6 +20,7 @@
 #include "ws2812_set_rgb.h"
 
 #include "util.h"
+#include "board.h"
 #include "constant.h"
 #include "enum.h"
 #include "pico_uart_transports/pico_uart_transports.h"
@@ -41,19 +42,6 @@ static status_t ping_agent(int timeout_ms, int nb_attempts) {
     return KO;
   return OK;
 }
-
-/*static void update_timer_callback(rcl_timer_t *timer, int64_t last_call_time)
-{
-  empile_trace("update_timer_callback");
-
-  for (int pin = 0; pin < NB_JOINTS; pin++) {
-    if (joints[pin].goal_angle != joints[pin].actual_angle) {
-      joints[pin].config.move_callback(&joints[pin]);
-    }
-  }
-
-  depile_trace("update_timer_callback");
-}*/
 
 static void uptime_timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
@@ -84,10 +72,9 @@ static void joint_subscriber_callback(const sensor_msgs__msg__JointState *inputs
 {
   empile_trace("servo_subscriber_callback");
 
-  for (size_t i = 0; i < NB_JOINTS; i++) {
-    joints[i].angle = joints_angle[i];
-    if (joints[i].config.move_callback != NULL) {
-      joints[i].config.move_callback(&joints[i]);
+  for (size_t i = 0; i < NB_BOARDS; i++) {
+    if (boards[i].joint_update != NULL) {
+      boards[i].joint_update(&boards[i], inputs);
     }
   }
 
@@ -120,8 +107,8 @@ status_t init_ros_pico(void) {
 
   //rclc_timer_init_default(&update_timer, &node.support, UPDATE_TIMER_INTERVAL, &update_timer_callback);
   rclc_timer_init_default(&uptime_timer, &node.support, UPTIME_TIMER_INTERVAL, &uptime_timer_callback);
-  rclc_publisher_init_default(&uptime_publisher, &node.node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), UPTIME_PUBLISHER_TOPIC_NAME);
   rclc_executor_add_timer(&node.executor, &uptime_timer);
+  rclc_publisher_init_default(&uptime_publisher, &node.node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), UPTIME_PUBLISHER_TOPIC_NAME);
 
   sensor_msgs__msg__JointState__init(&joint_subscriber_data);
   init_joint_subscriber_data();
